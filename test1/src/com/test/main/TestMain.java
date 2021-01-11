7a8ac82c937a36ee0e0fb7061688adbddfd07043
oofly1982 <406697588@qq.com>
2021-01-11 16:53:06 +0800

    test2

--- test1/src/com/test/main/TestMain.java
+++ test1/src/com/test/main/TestMain.java
@@ -3,6 +3,10 @@ package com.test.main;
 public class TestMain {
 
     public static void main(String[] args){
-        System.out.println("my test!!!");
+        System.out.println("my test2222!!!");
+        int a = 5;
+        int b = 10;
+        int c = a+b;
+        System.out.println("c: " + c);
     }
 }
